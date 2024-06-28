555d324b48b0f429190c14cda87f7a33159a758b
Tiago Pereira <tiagojopereira@gmail.com>
2024-06-28 13:14:36 +0200

    select image

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -7,154 +7,82 @@ import {
   Text,
   StyleSheet,
   SafeAreaView,
+  Platform,
+  Alert,
 } from "react-native"
 import * as ImagePicker from "expo-image-picker"
 // import { RNCamera, RNCameraProps } from "react-native-camera"
 // import WebSocket from "react-native-websockets"
 import axios from "axios"
 
-// Define types for detection results
-interface DetectionResult {
-  box: number[]
-  class_id: number
-  score: number
-}
-
-// const App: React.FC = () => {
-//   const [image, setImage] = useState<string | null>(null)
-//   const [loading, setLoading] = useState<boolean>(false)
-//   const [results, setResults] = useState<DetectionResult[]>([])
-
-//   const ws = useRef<WebSocket | null>(null)
-//   // const camera = useRef<RNCamera>(null)
-
-//   useEffect(() => {
-//     ws.current = new WebSocket("ws://your-server-ip:8765")
-//     ws.current.onmessage = (e: WebSocketMessageEvent) => {
-//       const response: DetectionResult[] = JSON.parse(e.data)
-//       setResults(response)
-//       setLoading(false)
-//     }
-//     return () => {
-//       if (ws.current) {
-//         ws.current.close()
-//       }
-//     }
-//   }, [])
-
-//   const pickImage = async () => {
-//     let result = await ImagePicker.launchImageLibraryAsync({
-//       mediaTypes: ImagePicker.MediaTypeOptions.Images,
-//       allowsEditing: true,
-//       aspect: [4, 3],
-//       quality: 1,
-//     })
-
-//     if (!result.canceled) {
-//       setImage(result.uri)
-//       setLoading(true)
-//       const formData = new FormData()
-//       formData.append("image", {
-//         uri: result.uri,
-//         type: "image/jpeg",
-//         name: "photo.jpg",
-//       })
-
-//       // Send the image to the server for processing
-//       axios
-//         .post("http://your-server-ip:5000/predict", formData, {
-//           headers: { "Content-Type": "multipart/form-data" },
-//         })
-//         .then((response) => {
-//           setResults(response.data)
-//           setLoading(false)
-//         })
-//         .catch((error) => {
-//           console.error(error)
-//           setLoading(false)
-//         })
-//     }
-//   }
-
-//   const handleCapture = async () => {
-//     if (camera.current) {
-//       setLoading(true)
-//       const options = { quality: 0.5, base64: true }
-//       const data = await camera.current.takePictureAsync(options)
-//       const imageBase64 = data.base64
-//       if (ws.current && ws.current.readyState === WebSocket.OPEN) {
-//         ws.current.send(JSON.stringify({ image: imageBase64 }))
-//       }
-//     }
-//   }
-
-//   return (
-//     <View style={styles.container}>
-//       {/* Button to pick an image from the gallery */}
-//       <Button title="Pick an image from camera roll" onPress={pickImage} />
-
-//       {/* Button to capture an image using the camera */}
-//       <Button title="Capture image with camera" onPress={handleCapture} />
-
-//       {/* Display the selected or captured image */}
-//       {image && (
-//         <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
-//       )}
-
-//       {/* Camera preview */}
-//       <RNCamera
-//         ref={camera}
-//         style={styles.preview}
-//         type={RNCamera.Constants.Type.back}
-//         captureAudio={false}
-//       />
-
-//       {/* Show loading indicator while processing */}
-//       {loading && <ActivityIndicator size="large" color="#0000ff" />}
-
-//       {/* Display the results from the server */}
-//       {results.length > 0 && (
-//         <View style={styles.results}>
-//           {results.map((result, index) => (
-//             <Text key={index} style={styles.resultText}>
-//               Class ID: {result.class_id}, Score: {result.score.toFixed(2)}
-//             </Text>
-//           ))}
-//         </View>
-//       )}
-//     </View>
-//   )
-// }
-
 const App = () => {
+  const [image, setImage] = useState<string | null>(null)
+  null
+
+  const pickImage = async () => {
+    let result
+    try {
+      result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+      })
+
+      console.log("ImagePicker Result:", result)
+
+      if (
+        !result.canceled &&
+        result.assets &&
+        Array.isArray(result.assets) &&
+        result.assets.length > 0
+      ) {
+        setImage(result.assets[0].uri)
+      } else {
+        console.log("Image picking was canceled or no assets found")
+      }
+    } catch (error) {
+      console.error("Error picking image: ", error)
+    }
+  }
+
   return (
-    <SafeAreaView>
-      <Text>App is running</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.innerContainer}>
+        {!image && <Text>Please upload your image</Text>}
+        {image && (
+          <View style={styles.imageContainer}>
+            <Image source={{ uri: image }} style={styles.image} />
+          </View>
+        )}
+        <Button
+          title={!image ? "Upload Image" : "Identify Image"}
+          onPress={!image ? pickImage : () => console.warn("Identify image")}
+        />
+        {image && <Button title={"Upload Image"} onPress={pickImage} />}
+      </View>
     </SafeAreaView>
   )
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  innerContainer: {
+    minHeight: 700,
     justifyContent: "center",
     alignItems: "center",
   },
-  preview: {
-    flex: 1,
-    justifyContent: "flex-end",
+  imageContainer: {
+    justifyContent: "center",
     alignItems: "center",
-    width: "100%",
+    padding: 2,
+    borderWidth: 1,
+    borderColor: "black",
+  },
+  image: {
+    width: 300,
     height: 300,
-    marginTop: 20,
-  },
-  results: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  resultText: {
-    fontSize: 18,
-    margin: 5,
   },
 })
 
